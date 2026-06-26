eddfa69c896a2afd8b5dcb09010b8425b8b2c660
Prateek Kumar Gupta <167278490+prateek-kumar-gupta@users.noreply.github.com>
2026-06-26 16:36:56 +0530

    Time: 76 ms (7.75%), Space: 151.3 MB (24.37%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,29 +1,73 @@
 class MinStack {
 public:
-   stack<pair<int,int>>st;
+//    stack<pair<int,int>>st;
   
-    MinStack() {
+//     MinStack() {
         
-    }
+//     }
     
-    void push(int value) {
-      if(st.empty())
-    st.push({value, value});
-else
-    st.push({value, min(value, st.top().second)});
-    }
+//     void push(int value) {
+//       if(st.empty())
+//     st.push({value, value});
+// else
+//     st.push({value, min(value, st.top().second)});
+//     }
     
+//     void pop() {
+//         st.pop();
+//     }
+    
+//     int top() {
+//         return st.top().first;
+//     }
+    
+//     int getMin() {
+//         return st.top().second;
+//             }
+    stack<long long> st;
+    long long mini;
+
+    MinStack() {
+
+    }
+
+    void push(int val) {
+
+        if(st.empty()) {
+            st.push(val);
+            mini = val;
+        }
+        else if(val >= mini) {
+            st.push(val);
+        }
+        else {
+            st.push(2LL * val - mini);
+            mini = val;
+        }
+    }
+
     void pop() {
+
+        if(st.top() < mini) {
+            mini = 2LL * mini - st.top();
+        }
+
         st.pop();
     }
-    
+
     int top() {
-        return st.top().first;
+
+        if(st.top() < mini)
+            return mini;
+
+        return st.top();
     }
-    
+
     int getMin() {
-        return st.top().second;
-            }
+
+        return mini;
+    }
+
 };
 
 /**
