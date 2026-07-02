2441bd7a12905c7bd235e227d58e04518a961312
Prateek Kumar Gupta <167278490+prateek-kumar-gupta@users.noreply.github.com>
2026-07-02 15:38:58 +0530

    Time: 0 ms (100%), Space: 22.1 MB (59.87%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,38 +1,82 @@
+// class Solution {
+// public:
+//     vector<int> asteroidCollision(vector<int>& astroids) {
+//         vector<int>ans ;
+//         stack<int>st ;
+//         int n = astroids.size();
+//         for(int i = 0 ; i < n ; i ++){
+//             if(astroids[i]>0){st.push(astroids[i]);}
+//             else{
+//                 if(st.empty()){ans.push_back(astroids[i]);}
+//                 else {
+//                     if(st.top()> -1*astroids[i]){
+//                         continue ;
+//                     }
+//                     else if(st.top()== -1*astroids[i]){
+//                         st.pop();
+//                         continue ;
+//                     }
+//                     else{
+//                         st.pop() ;
+//                         i--;
+                        
+//                     }
+//                 }
+//             }
+//         }
+//         stack<int>st1;
+//         while(!st.empty()){
+//             st1.push(st.top());
+//             st.pop();
+//         }
+//         while(!st1.empty()){
+//             ans.push_back(st1.top());
+//             st1.pop();
+//         }
+//         return ans ;
+//     }
+// };
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& astroids) {
-        vector<int>ans ;
-        stack<int>st ;
-        int n = astroids.size();
-        for(int i = 0 ; i < n ; i ++){
-            if(astroids[i]>0){st.push(astroids[i]);}
-            else{
-                if(st.empty()){ans.push_back(astroids[i]);}
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+
+        stack<int> st;
+        int n = asteroids.size();
+
+        for(int i = 0; i < n; i++) {
+
+            if(asteroids[i] > 0) {
+                st.push(asteroids[i]);
+            }
+            else {
+
+                if(st.empty() || st.top() < 0) {
+                    st.push(asteroids[i]);
+                }
                 else {
-                    if(st.top()> -1*astroids[i]){
-                        continue ;
+
+                    if(st.top() > -asteroids[i]) {
+                        continue;
                     }
-                    else if(st.top()== -1*astroids[i]){
+                    else if(st.top() == -asteroids[i]) {
                         st.pop();
-                        continue ;
+                        continue;
                     }
-                    else{
-                        st.pop() ;
+                    else {
+                        st.pop();
                         i--;
-                        
                     }
                 }
             }
         }
-        stack<int>st1;
-        while(!st.empty()){
-            st1.push(st.top());
+
+        vector<int> ans(st.size());
+
+        for(int i = st.size() - 1; i >= 0; i--) {
+            ans[i] = st.top();
             st.pop();
         }
-        while(!st1.empty()){
-            ans.push_back(st1.top());
-            st1.pop();
-        }
-        return ans ;
+
+        return ans;
     }
 };
