9ca3dcd7d601ac789ee3fb8fc00a4f07cc979e47
Prateek Kumar Gupta <167278490+prateek-kumar-gupta@users.noreply.github.com>
2026-07-09 15:25:08 +0530

    Time: 0 ms (100%), Space: 71.8 MB (28.59%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,3 +1,35 @@
+// class Solution {
+// public:
+//     int longestOnes(vector<int>& nums, int k) {
+
+//         int n = nums.size();
+
+//         int l = 0;
+//         int r = 0;
+//         int zeros = 0;
+//         int ans = 0;
+
+//         while(r < n){
+
+//             if(nums[r] == 0)
+//                 zeros++;
+
+//             while(zeros > k){
+
+//                 if(nums[l] == 0)
+//                     zeros--;
+
+//                 l++;
+//             }
+
+//             ans = max(ans, r - l + 1);
+
+//             r++;
+//         }
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
@@ -6,20 +38,18 @@ public:
 
         int l = 0;
         int r = 0;
-        int zeros = 0;
         int ans = 0;
+
+        queue<int> q;
 
         while(r < n){
 
             if(nums[r] == 0)
-                zeros++;
+                q.push(r);
 
-            while(zeros > k){
-
-                if(nums[l] == 0)
-                    zeros--;
-
-                l++;
+            if(q.size() > k){
+                l = q.front() + 1;
+                q.pop();
             }
 
             ans = max(ans, r - l + 1);
