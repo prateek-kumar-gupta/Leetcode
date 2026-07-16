43ce04e4d8849eb4e8b81b3c21c54d60391ff927
Prateek Kumar Gupta <167278490+prateek-kumar-gupta@users.noreply.github.com>
2026-07-16 15:40:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.21%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,10 +1,32 @@
+// class Solution {
+// public:
+//     int xorOperation(int n, int start) {
+//         int ans = 0 ;
+//         for(int i = 0 ; i < n ; i++){
+//             ans = ans^(start + 2*i);
+//         }
+//         return ans ;
+//     }
+// };
 class Solution {
 public:
+    int prefixXor(int x) {
+        if (x % 4 == 0) return x;
+        if (x % 4 == 1) return 1;
+        if (x % 4 == 2) return x + 1;
+        return 0;
+    }
+
     int xorOperation(int n, int start) {
-        int ans = 0 ;
-        for(int i = 0 ; i < n ; i++){
-            ans = ans^(start + 2*i);
-        }
-        return ans ;
+        int s = start >> 1;
+
+        int ans = prefixXor(s + n - 1) ^ prefixXor(s - 1);
+
+        ans <<= 1;
+
+        if ((n & 1) && (start & 1))
+            ans |= 1;
+
+        return ans;
     }
 };
