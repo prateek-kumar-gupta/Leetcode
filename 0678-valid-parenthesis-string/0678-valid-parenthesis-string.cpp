f6382897b106129cf912631a5ab183d7f063deb2
Prateek Kumar Gupta <167278490+prateek-kumar-gupta@users.noreply.github.com>
2026-07-27 12:10:17 +0530

    Time: 0 ms (100%), Space: 8 MB (75.08%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+
+        int low = 0, high = 0;
+
+        for(char c : s){
+
+            if(c == '('){
+                low++;
+                high++;
+            }
+            else if(c == ')'){
+                if(low > 0) low--;
+                high--;
+
+                if(high < 0)
+                    return false;
+            }
+            else{ // '*'
+                if(low > 0) low--;
+                high++;
+            }
+        }
+
+        return low == 0;
+    }
+};
