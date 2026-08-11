7136efe80a0b0a878650925459ae90851b32f99e
Prateek Kumar Gupta <167278490+prateek-kumar-gupta@users.noreply.github.com>
2026-08-11 23:20:46 +0530

    Time: 0 ms (100%), Space: 22.3 MB (18.65%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -9,25 +9,35 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void solve(TreeNode* root, int depth, int &ans) {
+
+//         if(root == NULL)
+//             return;
+
+//         ans = max(ans, depth);
+
+//         solve(root->left, depth + 1, ans);
+//         solve(root->right, depth + 1, ans);
+//     }
+
+//     int maxDepth(TreeNode* root) {
+
+//         int ans = 0;
+
+//         solve(root, 1, ans);
+
+//         return ans;
+//     }
+// };
 class Solution {
 public:
-    void solve(TreeNode* root, int depth, int &ans) {
-
-        if(root == NULL)
-            return;
-
-        ans = max(ans, depth);
-
-        solve(root->left, depth + 1, ans);
-        solve(root->right, depth + 1, ans);
-    }
-
     int maxDepth(TreeNode* root) {
+        if(root == NULL)
+            return 0;
 
-        int ans = 0;
-
-        solve(root, 1, ans);
-
-        return ans;
+        return 1 + max(maxDepth(root->left),
+                       maxDepth(root->right));
     }
 };
